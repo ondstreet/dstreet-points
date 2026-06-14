6dddea4deb0dd8a5ecc14e1fd71208260fade5d0
ondstreet <uondstreet@gmail.com>
2026-06-14 16:47:20 -0500

    Update run_points.py

--- run_points.py
+++ run_points.py
@@ -1,11 +1,11 @@
 from flask import Flask, render_template
 from points_service.points_api import points_bp
-from web.api.community_api import community_bp
+# from web.api.community_api import community_bp
 
 app = Flask(__name__)
 
 app.register_blueprint(points_bp)
-app.register_blueprint(community_bp)
+# app.register_blueprint(community_bp)
 
 @app.route('/')
 @app.route('/dashboard')
